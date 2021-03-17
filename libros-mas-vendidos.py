331d5ebdf08bae6c089e4f8ce25c5b9409678ec7
Alejandra-Cristina <amarrerosuarez@uoc.edu>
2021-03-17 16:35:39 +0000

    v1.0.5
    Añadido nuevos campos al excel y csv y acumulado de todas las paginas.

--- libros-mas-vendidos.py
+++ libros-mas-vendidos.py
@@ -1,29 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import os
+import pandas as pd
+            
+        
 # La función get_main_news retornará un diccionario con todas las urls, títulos y autor de libros encontrados en la sección principal.
-def get_main_news():
-    url = 'https://www.todostuslibros.com/mas_vendidos?page=1'
+def get_all_best_sellers_100():
+    
+    data = [];
+    for page_number in range(0,11):
+        url = 'https://www.todostuslibros.com/mas_vendidos?page='+str(page_number)
+        respuesta = launch_request(url)
 
-    respuesta = launch_request(url)
+        contenido_web = BeautifulSoup(respuesta.text, 'html.parser')
+        libreria = contenido_web.find('ul', attrs={'class':'books'})
+        libros = libreria.findChildren('div', attrs={'class':'book-details'})
 
-    contenido_web = BeautifulSoup(respuesta.text, 'lxml')
+        # Después recorreremos la lista para obtener la url, el título, para ello usaremos el siguiente código:
+        for libro in libros:            
+            
+            respuesta = launch_request(libro.find('h2').a.get('href'))
+            contenido_web = BeautifulSoup(respuesta.text, 'html.parser')
+              
+            datos = {
+                'url': libro.find('h2').a.get('href'),
+                'titulo': libro.find('h2').get_text().strip(),
+                'autor': libro.find('h3').get_text().strip(),
+                'materias': contenido_web.select_one('.materias a').get_text()
+            }
+            
+            #Obtenemos las dos columnas de informacion con editorial, peso del libro y otros atributos
+            informacion_relativa_libro = contenido_web.select('.col-12.col-sm-12.col-md-12.col-lg-6 > .row')
+            
+            for informacion_relativa_libro_i in informacion_relativa_libro:
+                keys = informacion_relativa_libro_i.find_all('dt')
+                values = informacion_relativa_libro_i.find_all('dd')
 
-# Buscar la etiqueta ul, que tenga la clase books, que es la lista que contiene todas las urls. 
-    libreria = contenido_web.find('ul', attrs={'class':'books'})
-# Con el resultado de esa búsqueda, utilizaremos findChildren para encontrar todos los divs que usen la clase que book-details que es la que contiene la url, el título y el autor,
-# esto nos devolverá una lista con todos los resultados encontrados.
-    libros = libreria.findChildren('div', attrs={'class':'book-details'})
-
-# Después recorreremos la lista para obtener la url, el título, para ello usaremos el siguiente código:
-    datos = [];
-    for libro in libros:
-        datos.append({
-            'url': libro.find('h2').a.get('href'),
-			'titulo': libro.find('h2').get_text(),
-			'autor' : libro.find('h3').get_text()
-        })
-
-    return datos
+                for i in range(0,len(keys)):
+                    datos[keys[i].get_text()] = values[i].get_text()
+                    
+            data.append(datos)  
+            #print('=================================')
+            #print(data)
+            #print('=================================')
+            #print('\n')
+    return data
 
 
 def launch_request(url):
@@ -42,11 +64,12 @@ def launch_request(url):
 
 
 if __name__ == '__main__':
-    datos = get_main_news()
+    datos = get_all_best_sellers_100()
+    df = pd.DataFrame(datos)
+    #print(df)
+  
+    # creación en excel en la raíz de usuario
+    df.to_excel('libros_mas_vendidos.xlsx')
+    df.to_csv('libros_mas_vendidos.csv')
 
-	
-    for libros_mas_vendidos in datos:
-        print('=================================')
-        print(libros_mas_vendidos)
-        print('=================================')
-		
+   
